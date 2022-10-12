575f9892da0fdc793ac9f30a3a4c06e743ddc891
enriquedev53 <enriquedev53@gmail.com>
2022-10-11 21:41:30 -0500

    chore(.gitignore): ignore .env

--- context/QuioscoProvider.jsx
+++ context/QuioscoProvider.jsx
@@ -79,6 +79,24 @@ const QuioscoProvider = ({children}) => {
 
   const colocarOrden = async (e) => {
     e.preventDefault()
+
+    try {
+      await axios.post('/api/ordenes', {pedido, nombre, total,
+      fecha: Date.now().toString()})
+      // Reset
+      setCategoriaActual(categorias[0])
+      setPedido([])
+      setNombre('')
+      setTotal(0)
+
+      toast.success('Pedido Realizado Correctamente')
+
+      setTimeout(() => {
+        router.push('/');
+      }, 3000);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return (
